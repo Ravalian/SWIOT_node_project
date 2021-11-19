fa4332c409da42496ee5f7e3d26b59fc0159644a
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2021-11-19 11:24:19 +0100

    new GET

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ const HOST = 'localhost'
 const PORT = 8888;
 
 const Gpio = require('onoff').Gpio;
-const led = new Gpio(20, 'out');
-const led1 = new Gpio(21, 'out');
-const led2 = new Gpio(26, 'out');
+const piLED1 = new Gpio(20, 'out');
+const piLED2 = new Gpio(21, 'out');
+const piLED3 = new Gpio(26, 'out');
 const motor = new Gpio(22, 'out');
 
 api.listen(PORT, () => console.log('API running at '+HOST+':'+PORT+'!'));
@@ -23,14 +23,12 @@ api.get('/', (req, res) => {
 })
 
 api.get('/turn_on', (req, res) => {
-    led.writeSync(1);
-
+    piLED1.writeSync(1);
     res.send('Turning on LED');
 })
 
 api.get('/turn_off', (req, res) => {
-    led.writeSync(0);
-
+    piLED1.writeSync(0);
     res.send('Turning off LED');
 })
 
@@ -45,17 +43,37 @@ api.get('/turn_off_motor', (req, res) => {
 })
 
 api.post('/turn_on_led_from_api', (req, res) => {
-    led.writeSync(1);
-    led1.writeSync(1);
-    led2.writeSync(1);
+    piLED1.writeSync(1);
+    piLED2.writeSync(1);
+    piLED3.writeSync(1);
     res.send("turned on led");
 })
 
 api.post('/turn_off_led_from_api', (req, res) => {
-    led.writeSync(0);
-    led1.writeSync(0);
-    led2.writeSync(0);
+    piLED1.writeSync(0);
+    piLED2.writeSync(0);
+    piLED3.writeSync(0);
     res.send("turned off led");
+})
+
+var isOn = false;
+
+api.get('/LED_on_off', (req, res) => {
+    
+    if(isOn = false){
+        piLED1.writeSync(1);
+        piLED2.writeSync(1);
+        piLED3.writeSync(1);
+        isOn = true;
+    } 
+    else {
+        piLED1.writeSync(0);
+        piLED2.writeSync(0);
+        piLED3.writeSync(0);
+        isOn = false;
+    }
+    
+    res.send("turned on led");
 })
 
 /*const app = http.createServer(
